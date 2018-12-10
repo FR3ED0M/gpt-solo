6ec717cb346eaa5158036aa00d9dda031c8c7ca2
FR3ED0M <lgarcia2013@fau.edu>
2018-12-10 11:19:47 -0500

    added gpts files

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    fire: {
+        apiKey: "AIzaSyDPBdEny-p33xWIkxQmZGV8LrJkDAb-sF4",
+        authDomain: "gpts-4bb71.firebaseapp.com",
+        databaseURL: "https://gpts-4bb71.firebaseio.com",
+        projectId: "gpts-4bb71",
+        storageBucket: "gpts-4bb71.appspot.com",
+        messagingSenderId: "136716938071" 
+    }
+};
